6d8b4efbec5519bb4106f9301e7bfc23dfc8fe1a
serzh <nechyporchuk@gmail.com>
2015-12-07 10:43:14 +0200

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject malcolmx "0.1.6-SNAPSHOT"
+(defproject malcolmx "0.1.6"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
